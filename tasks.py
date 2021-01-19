42222f8a9952e35b98f48c7607ddcbc22bb8706b
Max Kharandziuk <max.kharandziuk@gmail.com>
2021-01-19 21:22:32 +0100

    remove the dependency task

--- tasks.py
+++ tasks.py
@@ -8,11 +8,6 @@ load_dotenv()
 
 
 @task
-def dependencies(c):
-    c.run("pip install -r requirements.txt")
-
-
-@task(dependencies)
 def validate_code(c):
     c.run("pre-commit install")
     c.run("pre-commit run --all-files")
