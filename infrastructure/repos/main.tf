eb14ad586c02ad733b128e71e76941b6316582f4
Max Kharandziuk <max.kharandziuk@gmail.com>
2021-01-19 22:39:56 +0100

    add infrastructure code

---
+++ infrastructure/repos/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "-/aws"
+      version = "< 3.0"
+    }
+  }
+}
+
+variable "aws_region" {
+  default = "eu-central-1"
+  type    = string
+}
+
+variable "backend_service_name" {
+  type    = string
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecr_repository" "backend" {
+  name                 = var.backend_service_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "backend_repo_url" {
+  value = aws_ecr_repository.backend.repository_url
+}
